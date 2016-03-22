56ce4f8a6fbd0522be9ccf6f4d78db832ba638bd
poly <poly@darkdepths.net>
2016-03-22 16:39:30 +0400

    cover DataURI to binary conversion

--- webpages/upload.js
+++ webpages/upload.js
@@ -2,7 +2,8 @@
 
 PDFJS.workerSrc = "pdf.worker.js";
 
-function PDFtoPNG (pdfLocation) {
+function generateThumbURI (pdfLocation) {
+    // returns base64/URLEncoded thumnail from a PDF file
     PDFJS.getDocument(pdfLocation).then(function(pdf) {
         pdf.getPage(1).then(function(page) {
             var scale = 0.5;
@@ -30,4 +31,21 @@ function PDFtoPNG (pdfLocation) {
             });
         });
     });
+}
+
+function dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ia], {type:mimeString});
 }
